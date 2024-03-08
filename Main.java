333a5174ca827ea522b3a86dc66042cd8748e1c2
Paolo Conti <152903837+PaolettoCo@users.noreply.github.com>
2024-03-08 15:40:31 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Animale a = new Animale();
+        Gatto g = new Gatto();
+
+        a.faiIlVerso();
+        g.faiIlVerso();
+
+    }
+}
